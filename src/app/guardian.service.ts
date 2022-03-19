25cbbb481a3e7f93bfe2ec0d08a68b4ffc0084d8
StLewy <52931319+StLewy@users.noreply.github.com>
2022-03-19 12:07:23 +0100

    add needy functionality

--- src/app/guardian.service.ts
+++ src/app/guardian.service.ts
@@ -1,20 +1,22 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from 'rxjs';
+import {Needy} from "./models/needy";
+import {Code} from "./models/code"
 
-import {HttpClient, HttpParams,HttpHeaders} from "@angular/common/http";
-import { Observable, throwError } from 'rxjs';
-import { map, catchError} from 'rxjs/operators';
-import {response} from "express";
 @Injectable()
 export class GuardianService {
 
-  // baseURL:string = "http://localhost:8080/";
-  baseURL:string = "https://guardian-backend-api.herokuapp.com/";
+  baseURL:string = "http://localhost:8080";
+  // baseURL:string = "https://guardian-backend-api.herokuapp.com/";
 
   constructor(private http: HttpClient) {
   }
 
-  getCode(): Observable<any>{
-    return this.http.get(this.baseURL + 'code/generate',{responseType:'text'});
+  public getCode(): Observable<Code>{
+    return this.http.get<Code>(`${this.baseURL}/code/generate`);
   }
-
+  public getAllNeedByStatus(): Observable<Needy[]> {
+    return this.http.get<Needy[]>(`${this.baseURL}/needy/all-by-status`);
+  }
 }
---
+++ src/app/models/guardian.ts
@@ -0,0 +1,9 @@
+export class Guardian {
+  id:bigint | undefined;
+  name:string | undefined;
+  surname:string | undefined;
+  phone:string | undefined;
+  mail:string | undefined;
+  creation:string | undefined;
+  update:string | undefined;
+}
